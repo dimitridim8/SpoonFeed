e1ca921eb73a827e3fe5465260e0fe8e2107f05a
dimitridim8 <116752261+dimitridim8@users.noreply.github.com>
2024-12-30 20:24:21 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import RecipeDisplay from './RecipeDisplay';
 import Register from './Register';
 import Login from './Login';
-import LogMeal from './LogMeal'; // Import the LogMeal component
-import './App.css';
+import LogMeal from './LogMeal';
 import MealLog from './MealLog';
+import './App.css';
 
 function App() {
-  const location = useLocation(); // Hook to get the current location
+  const location = useLocation(); // Hook to detect current route
 
   return (
     <div className="App">
       <header className="app-header">
         <h1 className="app-heading">Welcome to SpoonFeed</h1>
         <nav className="nav-links">
-          {location.pathname === '/register' || location.pathname === '/login' || location.pathname === '/log-meal' ? (
-            <Link to="/" className="home-button">Home</Link> // Show Home link when on /log-meal
+          {location.pathname !== '/' ? (
+            <Link to="/" className="home-button">Home</Link>
           ) : (
-            <Link to="/log-meal" className="log-meal-button">Log Meal</Link> // Show Log Meal link otherwise
+            <Link to="/log-meal" className="log-meal-button">Log Meal</Link>
           )}
         </nav>
       </header>
+
       <main>
-      <Routes>
-        <Route path="/" element={<RecipeDisplay />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/log-meal" element={<LogMeal />} />
-        <Route path="/meal-logs" element={<MealLog />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<RecipeDisplay />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/log-meal" element={<LogMeal />} />
+          <Route path="/meal-logs" element={<MealLog />} />
+        </Routes>
       </main>
     </div>
   );
 }
 
-function AppWrapper() {
-  return (
-    <Router>
-      <App />
-    </Router>
-  );
-}
-
-export default AppWrapper;
+export default App;
